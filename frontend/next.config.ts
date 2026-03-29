7a754f189970730b8a94faa0222f4a87dc9a32fc
Aanish-Bangre <blueverse20@gmail.com>
2026-03-29 15:20:39 +0530

    feat(docker): add Dockerfile for backend and frontend services with multi-stage builds

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   allowedDevOrigins: [
     'http://localhost:3000',
   ]
